3afc729533eb5956f5f191815acc225260205300
Haoyuan Li <vamirio@qq.com>
2021-07-23 19:46:22 +0800

    update class File, add operations to deal with dir

--- File.cpp
+++ File.cpp
@@ -4,65 +4,272 @@
  * contains basic file operations
  *
  * Created by Haoyuan Li on 2021/07/21
- * Last Modified: 2021/07/22 00:06:22
+ * Last Modified: 2021/07/23 19:45:36
  */
 
 #include "File.hpp"
 
 using string = std::string;
+using strvec = std::vector<std::string>;
 
 File::File()
 {
 }
 
-string::size_type File::find_last_delimiter()
+/**
+ * \brief Open a file by a specific mode
+ * \param filename The filename
+ * \param mode The specific mode, same as stdandard function fopen()
+ * \return 0 when succeeded and -1 when failed
+ * \sa open()
+ * \sa close()
+ */
+int File::open_file(const string &filename, const string &mode)
 {
-        return fname.find_last_of("\\/");
-}
-
-int File::open(const string &filename, const string &mode)
-{
-        if (!(fp = fopen(filename.c_str(), mode.c_str())))
+        if (!(fp = fopen(filename.c_str(), mode.c_str()))) {
+                fprintf(stderr, "[error] couldn't open file %s\n",
+                                filename.c_str());
                 return -1;
-        fname = filename;
+        }
+        this->filename = get_filename_without_path(filename);
         return 0;
 }
 
+/**
+ * \brief Open a directory, then get all files in it
+ * \param dirname The directory name
+ * \return 0 when secceeded and -1 when failed
+ * \sa open()
+ * \sa close()
+ */
+int File::open_dir(const string &dirname)
+{
+        if (!(dp = opendir(dirname.c_str()))) {
+                fprintf(stderr, "[error] couldn't open directory %s\n",
+                                dirname.c_str());
+                return -1;
+        }
+        path = dirname;
+        index = 0;
+        return 0;
+}
+
+/**
+ * \brief Check if the file exists
+ * \param filename The filename
+ * \return true when the file exists and false when not
+ */
+bool File::exists(const string &filename)
+{
+        struct stat st;
+        return !(stat(filename.c_str(), &st));
+}
+
+/**
+ * \brief Get the last delimiter in the path
+ * \return The index of the last delimiter, std::string::npos when not found
+ */
+string::size_type File::find_last_delimiter(const string &filename)
+{
+        return filename.find_last_of("/\\");
+}
+
+/**
+ * \brief Get the file type
+ * \param filename The filename
+ * \return IS_REG_FILE when it's a regular file, IS_DIR when it's a directory,
+ * UNKNOWN when it can't be recognized
+ */
+int File::get_type(const string &filename)
+{
+        struct stat st;
+        int type;
+        lstat(filename.c_str(), &st);
+        if (S_ISREG(st.st_mode))
+                type = IS_REG_FILE;
+        else if (S_ISDIR(st.st_mode))
+                type = IS_DIR;
+        else
+                type = UNKNOWN;
+        return type;
+}
+
+/**
+ * \brief move to the previous file in the current directory
+ */
+void File::move_to_prev_file()
+{
+        index = (index == 0) ? index : index - 1;
+        open_file(file_list[index], "r");
+}
+
+/**
+ * \brief Get the current filename
+ * \return The current filename
+ */
+string File::get_curr_file()
+{
+        return file_list[index];
+}
+
+/**
+ * \brief move to the next file in the current directory
+ */
+void File::move_to_next_file()
+{
+        index = (index == file_list.size() - 1 ? index : index + 1);
+        open_file(file_list[index], "r");
+}
+
+/**
+ * \brief Open a file and the directory it locates
+ * \param filename The filename
+ * \return 0 when succeeded, -1 when failed
+ * \sa open_file()
+ * \sa open_dir()
+ * \sa close()
+ */
+int File::open(const string &filename)
+{
+        int type = get_type(filename);
+        if (type == IS_REG_FILE) {
+                open_file(filename, "r");
+                open_dir(get_path(filename));
+                get_file_list();
+        } else if (type == IS_DIR) {
+                open_dir(filename);
+                get_file_list();
+                open_file(file_list[0], "r");
+        } else {
+                fprintf(stderr, "[error] unknown type: %s\n",
+                                filename.c_str());
+                return -1;
+        }
+        return 0;
+}
+
+/**
+ * \brief Close this file and directory
+ * \sa open()
+ */
 void File::close()
 {
         fclose(fp);
-        fname = "";
+        closedir(dp);
 }
 
+/**
+ * \brief Open a file by a specific mode, then open the directory it locates
+ * \param filename The filename
+ * \param mode The specific mode, same as stdandard function fopen()
+ * \return 0 when succeeded and -1 when failed
+ * \sa open()
+ * \sa close()
+ */
+int File::open(const string &filename, const string &mode)
+{
+        if(open_file(filename, mode) || open_dir(get_path(filename))) {
+                fprintf(stderr, "[error] couldn't open %s\n",
+                                filename.c_str());
+                return -1;
+        }
+        get_file_list();
+        return 0;
+}
+
+/**
+ * \brief get the full filename
+ */
 string File::get_full_filename()
 {
-        return fname;
+        return path + "/" + filename;
 }
 
+/**
+ * \brief Get the file extension
+ * \return the extension, "" if not found
+ */
 string File::get_ext()
 {
-        string ext{fname};
-        auto pos_delim = find_last_delimiter();
+        string ext{filename};
         auto pos = ext.find_last_of('.');
-        if (pos != std::string::npos &&
-                        (pos_delim < pos ||
-                         pos_delim == std::string::npos))
+        if (pos != std::string::npos)
                 return ext.substr(pos + 1);
         return "";
 }
 
+/**
+ * \brief Get filename without path
+ * \return The filename without path
+ */
 string File::get_filename_without_path()
 {
-        auto pos = find_last_delimiter();
-        if (pos != std::string::npos)
-                return fname.substr(pos + 1);
-        return fname;
+        return filename;
 }
 
+/**
+ * \brief Get filename without path
+ * \param filename The filename
+ * \return The filename without path
+ */
+string File::get_filename_without_path(const string &filename)
+{
+        auto pos = find_last_delimiter(filename);
+        if (pos != std::string::npos)
+                return filename.substr(pos + 1);
+        return filename;
+}
+
+/**
+ * \brief Get the current path
+ * \return The path
+ */
 string File::get_path()
 {
-        auto pos = find_last_delimiter();
+        return path;
+}
+
+/**
+ * \brief Get the current path
+ * \param filename The filename
+ * \return The path
+ */
+string File::get_path(const string &filename)
+{
+        auto pos = find_last_delimiter(filename);
         if (pos != std::string::npos)
-                return fname.substr(0, pos);
-        return "";
+                return filename.substr(0, pos);
+        return ".";
+}
+
+/**
+ * \brief Get the file list in the current directory
+ * \return The file list
+ */
+strvec File::get_file_list()
+{
+        DIR *tmp = dp;
+        struct dirent *p;
+
+        while ((p = readdir(tmp))) {
+                if (string{"."} == p->d_name || string{".."} == p->d_name)
+                        continue;
+                file_list.push_back(p->d_name);
+        }
+        std::sort(file_list.begin(), file_list.end());
+        return file_list;
+}
+
+/**
+ * \brief Change the current directory
+ * \param dirname The destination directory
+ */
+void File::cd(const std::string &dirname)
+{
+        if (chdir(dirname.c_str())) {
+                fprintf(stderr, "[error] couldn't change current directory to %s\n", dirname.c_str());
+                return;
+        }
+        close();
+        open(dirname);
 }
--- File.hpp
+++ File.hpp
@@ -4,19 +4,39 @@
  * contains basic file operations
  *
  * Created by Haoyuan Li on 2021/07/21
- * Last Modified: 2021/07/22 00:05:56
+ * Last Modified: 2021/07/23 19:44:50
  */
+
+#ifndef FILE_HPP
+#define FILE_HPP
 
 #include <cstdio>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define IS_REG_FILE 0
+#define IS_DIR 1
+#define UNKNOWN 2
 
 class File {
 private:
-        std::string fname;
-        FILE *fp;
+        // use for file
+        std::string filename;   // the current filename
+        FILE *fp;       // point to the current file
+
+        // use for directory
+        std::string path;       // the current path
+        DIR *dp;        // point to the current directory
+        std::vector<std::string> file_list;
+        size_t index;   // the current file index
 
 private:
-        std::string::size_type find_last_delimiter();
+        int open_file(const std::string &filename, const std::string &mode);
+        int open_dir(const std::string &dirname);
 
 public:
         File();
@@ -25,10 +45,28 @@ public:
         File &operator=(const File &) = delete;
 
 public:
-        int open(const std::string &filename, const std::string &mode);
+        // common
+        bool exists(const std::string &filename);
+        std::string::size_type find_last_delimiter(const std::string &filename);
+        int get_type(const std::string &filename);
+        void move_to_prev_file();
+        std::string get_curr_file();
+        void move_to_next_file();
+        int open(const std::string &filename);
         void close();
+
+        // use for file
+        int open(const std::string &filename, const std::string &mode);
         std::string get_full_filename();
         std::string get_ext();
         std::string get_filename_without_path();
+        std::string get_filename_without_path(const std::string &filename);
         std::string get_path();
+        std::string get_path(const std::string &filename);
+
+        // use for directory
+        std::vector<std::string> get_file_list();
+        void cd(const std::string &dirname);
 };
+
+#endif
